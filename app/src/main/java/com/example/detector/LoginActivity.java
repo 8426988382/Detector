a21d2404d377e9f48075364e5144eec69327a7ef
Rishab Aggarwal <rishabaggarwal247@gmail.com>
2020-11-15 15:20:28 +0530

    edittext color fix

--- app/src/main/java/com/example/detector/LoginActivity.java
+++ app/src/main/java/com/example/detector/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.detector;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
